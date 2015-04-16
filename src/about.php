9a6feb629bad2a593294ed6d822aaccb8201a91c
Raven Smith <rds5gg@labunix01.cs.virginia.edu>
2015-04-16 18:09:24 -0400

    changed about from html to php for sessioning

---
+++ src/about.php
@@ -0,0 +1,81 @@
+<?php
+include('session.php');
+
+        $connection = mysqli_connect("stardock.cs.virginia.edu", "cs4750pnl8zp", "hospital", "cs4750pnl8zp");
+        if (!$connection) {
+	        //echo "connection failed";
+        } else {
+        	//echo "connection successful";
+        	}
+        	
+session_start();// Starting Session
+
+$query = mysqli_query($connection, "select * from Patient where userid='$ref_id'");
+$patient_info = mysqli_fetch_row($query);
+
+
+mysqli_close($connection); // Closing Connection
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap core CSS -->
+    <link href="static/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="base-template.css" rel="stylesheet">
+
+    <!-- JQuery -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+
+    <title>CS 4750 - Group Project</title>
+  </head>
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.html">Hospital DB</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="lookup.php">Search</a></li>
+            <li class="active"><a href="#">About</a></li>
+            <li><a href="signin.php">Sign in</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <div class="starter-template">
+        <h1>CS 4750 - Database Systems</h1>
+        <p class="lead">Semester Project Contributors</p>
+	<p>Philip Liberato (pnl8zp)</p>
+	<p>Raven Smith (rds5gg)</p>
+	<p>Ismail Wahid (iaw4ay)</p>
+	<p>Scott Newton (stn4at)</p>
+      </div>
+
+    </div><!-- /.container -->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="static/js/bootstrap.min.js"></script>
+
+  </body>
+</html>
