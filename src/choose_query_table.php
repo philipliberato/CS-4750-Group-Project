40f89e504e7a9b899933cdf88fd2788381b8c8c6
pnl8zp <pnl8zp@labunix01.cs.virginia.edu>
2015-04-16 21:02:42 -0400

    added select table form

--- src/choose_query_table.php
+++ src/choose_query_table.php
@@ -1,11 +1,17 @@
 <?php
 include('session.php');
-?>
 
-<br>
-<h3 id="resultTxt">Choose Query Table</h3>
-<br>
-<form action="">
-<input id="resultTxt" type="radio" name="table" value="Doctor">Doctor<br>
-<input id="resultTxt" type="radio" name="table" value="Nurse">Nurse<br>
-</form>
+echo "<br><h3 id=\"resultTxt\">Choose Query Table</h3><br>";
+
+$table_names = array("Administrator", "Appointment", "Department", "Doctor", "Employee", "Medication", "Nurse", "Occupies", "Patient", "Pharmacist", "Receptionist", "Record", "Room", "User");
+
+echo "<form action=\"\">";
+
+foreach ($table_names as $value) {
+    echo "<input id=\"resultTxt\" type=\"radio\" name=\"Table\" value=\"$value\">$value<br>";
+}
+
+echo "<br><input type=\"submit\" value=\"Submit\">";
+echo "</form>";
+
+?>
