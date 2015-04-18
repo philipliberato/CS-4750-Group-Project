67285a0fa5b504a6ff966cc4da72c72e755e9b5a
pnl8zp <pnl8zp@labunix01.cs.virginia.edu>
2015-04-18 19:05:18 -0400

    minimal changes

--- src/interface.php
+++ src/interface.php
@@ -1,6 +1,21 @@
 <?php
 include('session.php');
 
+        $connection = mysqli_connect("stardock.cs.virginia.edu", "cs4750pnl8zp", "hospital", "cs4750pnl8zp");
+        if (!$connection) {
+	        //echo "connection failed";
+        } else {
+        	//echo "connection successful";
+        	}
+        	
+session_start();// Starting Session
+
+$query = mysqli_query($connection, "select * from Employee where userid='$ref_id'");
+$employee_info = mysqli_fetch_row($query);
+
+
+mysqli_close($connection); // Closing Connection
+
 $Operation = $_GET["op"];
 $Table = $_POST["Table"];
 
@@ -68,7 +83,7 @@ function loadDeletePage() {
 	        <br>
 	        <br>
 		<?php
-	        echo $Operation;
+	        echo "<h3 id=\"resultTxt\">"$Operation;
 		echo "<br>";
 		echo $Table;
 	        ?>
