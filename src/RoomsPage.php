361d9de974cd1fba23af9361952a688ee73569ef
iaw4ny <iaw4ny@labunix03.cs.virginia.edu>
2015-04-22 10:15:56 -0400

    Adding RoomsPage.php

---
+++ src/RoomsPage.php
@@ -0,0 +1,42 @@
+<?php
+//$AuthorName = "Gandhi";
+
+
+$servername = "stardock.cs.virginia.edu";
+$username = "cs4750pnl8zp";
+$password = "hospital";
+$dbname = "cs4750pnl8zp";
+
+
+//$Table = "Doctor";
+//echo $Table;
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+
+
+
+$sql = "SELECT * FROM Room";
+$result = $conn->query($sql);
+
+
+if ($result->num_rows > 0) {
+
+ echo "<table>";
+       echo "<tr><th> RoomNumber </th></tr>";
+       while($row = $result->fetch_assoc()) {
+           echo "<tr>";
+           echo("<td>" . $row["RoomNumber"] . "</td>\n");
+           echo "</tr>";
+       }
+       echo "</table>";
+
+}
+
+
+?>
